763eada12897155f697b375035131f17fe6336e0
øllen <30601311+oliverwk@users.noreply.github.com>
2023-07-30 23:39:09 +0200

    Initial Commit

---
+++ TrainTrackLive/TrainTrackLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainTrackLiveApp.swift
+//  TrainTrackLive
+//
+//  Created by Maarten Wittop Koning on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrainTrackLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
