1f4888583fa9bdbaef94175b9b74aa95f6fee5d0
øllen <30601311+oliverwk@users.noreply.github.com>
2024-04-06 12:29:21 +0200

    Initial Commit

---
+++ TrackTrain/TrackTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTrainApp.swift
+//  TrackTrain
+//
+//  Created by Maarten Wittop Koning on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
