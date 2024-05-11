6447c3bc32599877293c024887916890e849db2a
øllen <30601311+oliverwk@users.noreply.github.com>
2024-05-11 12:10:17 +0200

    Added a Re button to reload the selectedRoute wiht most up to date information form the websocket.locations

--- TrackTrain/BottomView.swift
+++ TrackTrain/BottomView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 import MapKit
 
 struct BottomView: View {
-    var selectedRoute: LocationTrain?
+    @Binding var selectedTrajectory: [CLLocationCoordinate2D]?
+    @Binding var selectedRoute: LocationTrain?
+    @Binding var selectedStops: TrainStopContent?
     @Binding var visibleRegion: MKCoordinateRegion?
     @Binding var position: MapCameraPosition
+    @Binding var selectedItem: String?
     @ObservedObject var websocket: TrainWebsocket
     
     var body: some View {
         VStack {
-            if selectedRoute?.stops != nil {
-                Text("\(selectedRoute?.name ?? "No train name") from \(selectedRoute?.stops?.stations[0].stationName ?? "waiting") arriving at \(getArriTime(selectedRoute?.stops?.stations.last?.arrivalTime ?? 1000)) in \(selectedRoute?.stops?.destination ?? "waiting")")
+            if selectedRoute != nil {
+                Text("\(selectedRoute?.name ?? "No train name") from \(selectedStops?.stations[0].stationName ?? "waiting") arriving at \(getArriTime(selectedStops?.stations.last?.arrivalTime ?? 1000)) in \(selectedStops?.destination ?? "waiting")")
                         .padding(10)
                         .background(.thinMaterial)
                         .tint(.blue)
@@ -35,6 +38,18 @@ struct BottomView: View {
                             let LM = LocationModel()
                             LM.requestPermission()
                             position = .userLocation(fallback: .automatic)
+                        }.padding(.top)
+                        Button("Re") {
+                            let trainIndex = websocket.locations.firstIndex(where: {$0.id == selectedItem})
+                            print("index\(String(describing: trainIndex)) locations.id: \(websocket.locations[trainIndex!].id)  and selectedRoute.id: \(String(describing: selectedRoute?.id))")
+                            print("index\(String(describing: trainIndex)) locations.stop: \(String(describing: websocket.locations[trainIndex!].stops)) and selectedRoute.stops: \(String(describing: selectedRoute?.stops))")
+                            print("selectedRoute.stops:", selectedRoute?.stops ?? "nil")
+                            print("selectedRoute.trajectory: \(selectedRoute?.trajectory.count ?? 999) or \(selectedRoute?.trajectoryExpanded.count ?? 999)")
+                            selectedStops = websocket.locations[trainIndex!].stops
+                            selectedTrajectory = websocket.locations[trainIndex!].trajectory
+                            selectedRoute?.name = websocket.locations[trainIndex!].name
+                            print("selectedTrajectory: \(selectedTrajectory?.count ?? 999)")
+                            print("websocket.locations[trainIndex!].trajectory: \(websocket.locations[trainIndex!].trajectory.count) or \(websocket.locations[trainIndex!].trajectoryExpanded.count)")
                         }.padding(.top)
                     }
                 }
---
+++ TrackTrain/TrainTrajectory.swift
@@ -0,0 +1,8 @@
+//
+//  TrainTrajectory.swift
+//  TrackTrain
+//
+//  Created by Maarten Wittop Koning on 12/04/2024.
+//
+
+import Foundation
